6c800adc43b9085d345c1b6a16fc858facb10723
Aleksandr Ponkratov <aleksandr.ponkratov@lamoda.ru>
2024-10-28 12:00:00 +0300

    Первая версия компонента

---
+++ oscript-component/KafkaMessage.cs
@@ -0,0 +1,67 @@
+﻿using Confluent.Kafka;
+using OneScript.Contexts;
+using OneScript.StandardLibrary.Collections;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using System.Text;
+
+namespace oscriptcomponent
+{
+    /// <summary>
+	/// Класс СообщениеКафка. Является представлением сообщения Kafka.
+	/// </summary>
+    [ContextClass("СообщениеКафка", "KafkaMessage")]
+    internal class KafkaMessage : AutoContext<KafkaMessage>
+    {
+        /// <summary>
+		/// Ключ.
+		/// </summary>
+		[ContextProperty("Ключ")]
+        public string Key { get; set; }
+
+        /// <summary>
+		/// Значение.
+		/// </summary>
+		[ContextProperty("Значение")]
+        public string Value { get; set; }
+
+        /// <summary>
+		/// Заголовки.
+		/// </summary>
+		[ContextProperty("Заголовки")]
+        public MapImpl Headers { get; set; }
+
+        public KafkaMessage(Message<string, string> message)
+        {
+            Key = message.Key;
+            Value = message.Value;
+            Headers = new MapImpl();
+
+            if (message.Headers.Count == 0) return;
+            foreach (var header in message.Headers)
+            {
+	            Headers.Insert(ValueFactory.Create(header.Key), ValueFactory.Create(Encoding.ASCII.GetString(header.GetValueBytes())));
+            }
+        }
+
+        public KafkaMessage(string value, string key = "", MapImpl? headers = null)
+        {
+            Key = key;
+            Value = value;
+            Headers = headers ?? new MapImpl();
+        }
+
+        /// <summary>
+		/// По умолчанию
+		/// </summary>
+        /// <param name="value">Значение</param>
+        /// <param name="key">Ключ</param>
+        /// <param name="headers">Заголовки</param>
+		/// <returns>СообщениеКафка</returns>
+        [ScriptConstructor]
+        public static KafkaMessage Constructor(string value, string key = "", MapImpl? headers = null)
+        {
+            return new KafkaMessage(value, key, headers);
+        }
+    }
+}
---
+++ oscript-component/KafkaProducer.cs
@@ -0,0 +1,75 @@
+﻿using Confluent.Kafka;
+using OneScript.Contexts;
+using OneScript.StandardLibrary.Collections;
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using System.Text;
+
+namespace oscriptcomponent
+{
+    /// <summary>
+	/// Класс КафкаПродюсер. Служит для отправки сообщений в топики Kafka.
+	/// </summary>
+    [ContextClass("КафкаПродюсер", "KafkaProducer")]
+    public class KafkaProducer : AutoContext<KafkaProducer>
+    {
+        private readonly IProducer<string, string> _confluentProducer;
+
+        /// <summary>
+		/// СписокБрокеров.
+		/// </summary>
+		[ContextProperty("СписокБрокеров")]
+        public string BrokerList { get; }
+
+        public KafkaProducer(string brokerList)
+        {
+            BrokerList = brokerList;
+            
+            var confluentProducerConfig = new ProducerConfig { BootstrapServers = BrokerList };
+            var confluentProducer = new ProducerBuilder<string, string>(confluentProducerConfig).Build();
+
+            _confluentProducer = confluentProducer;
+        }
+
+        /// <summary>
+		/// По умолчанию
+		/// </summary>
+        /// <param name="brokerList">СписокБрокеров</param>
+		/// <returns>КафкаПродюсер</returns>
+        [ScriptConstructor]
+        public static KafkaProducer Constructor(string brokerList = "")
+        {
+            return new KafkaProducer(brokerList);
+        }
+
+        /// <summary>
+        /// Отправить сообщение в топик 
+        /// </summary>
+        /// <param name="topic">Топик</param>
+        /// <param name="key">Ключ</param>
+        /// <param name="message">Сообщение</param>
+        /// <param name="headers">Заголовки</param>
+        [ContextMethod("Отправить", "Send")]
+        public void Produce(string topic, string key, string message, MapImpl? headers = null)
+        {
+            var confluentHeaders = new Headers();
+
+            if (headers != null)
+            {
+                foreach (var kv in headers)
+                {
+                    confluentHeaders.Add(kv.Key.AsString(), Encoding.ASCII.GetBytes(kv.Value.AsString()));
+                }
+            }
+
+            var confluentMessage = new Message<string, string>
+            {
+                Key = key,
+                Value = message,
+                Headers = confluentHeaders
+            };
+
+            _confluentProducer.Produce(topic, confluentMessage);
+        }
+    }
+}
